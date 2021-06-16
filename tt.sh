b8cb331357587e9640adfeca74c52366071e8652
Eric Tien <erictien02@gmail.com>
2021-06-16 22:37:37 +0800

    update lights output

---
+++ tt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm predict.txt
+mv ../../../Downloads/predict.txt ./
--- turning_light_simulator.py
+++ turning_light_simulator.py
@@ -1,17 +1,61 @@
+from os import stat
 import time
 # import tkinter as tk
 from tkinter import *
+from typing import Sized, TextIO
 
 # Build the Frame
 root = Tk()
 root.title('BE-Lab  turn signal')
-root.geometry("480x250")
+root.geometry("800x460")
+
+def turn_light(predict):
+    lights = []
+    left_lights = [0, 0, -1, 0, 0]
+    right_lights = [2, 2, -1, 2, 2]
+    radius = 10
+    step = len(left_lights)
+    j = radius
+    while j < len(predict):
+        candidate = predict[j-radius : j]
+        p = max(set(candidate), key=candidate.count)
+        if p == 1:
+            lights.extend([1 for _ in range(step)])
+        elif p == 0:
+            lights.extend(left_lights)
+        elif p == 2:
+            lights.extend(right_lights)
+        else:
+            raise ValueError("Illegal predict!!")
+        j += step
+    return lights
+
 
 count = 0
-prediction = [i % 3 for i in range(50)]
+# prediction = [i % 3 for i in range(50)]
+# prediction = [(i//10)%3 for i in range(50)]
+raw_predict = []
+with open('predict.txt', 'r') as f:
+    line = f.readline().strip('\n').split(',')
+    for p in line:
+        try:
+            raw_predict.append(int(p))
+        except:
+            pass
+print(f"len(raw_predict) = {len(raw_predict)}, total {len(raw_predict)/10} secs")
+lights = turn_light(raw_predict)
+prediction = [1 for _ in range(2)]
+prediction.extend(raw_predict)
+print(len(prediction))
+print(f"len(turn_lights) = {len(lights)}, total {len(lights)/10} secs")
+
+
+top_frame = Frame(root)
+top_frame.pack(side=TOP)
+
 
 frame = Frame(root)
-frame.pack(pady=32)
+frame.pack(pady=64)
 
 but_frame = Frame(root)
 but_frame.pack(side=BOTTOM)
@@ -23,7 +67,7 @@ button_l = Button(frame,
                    font = ('Comic Sans MS', 22),
                    width=5, height=2,
                    command = None)
-button_l.grid(row=0, column=0, padx=20)
+button_l.grid(row=0, column=0, padx=64)
 
 button_m = Button(frame,
                    text = '\U0001f815\nfront',
@@ -31,7 +75,7 @@ button_m = Button(frame,
                    font = ('Comic Sans MS', 22),
                    width=5, height=2,
                    command = None)
-button_m.grid(row=0, column=1, padx=20)
+button_m.grid(row=0, column=1, padx=64)
 
 button_r = Button(frame,
                    text = '\U0001f816\nright',
@@ -40,10 +84,12 @@ button_r = Button(frame,
                    width=5, height=2,
                    command = None)
 
-button_r.grid(row=0, column=2, padx=20)
+button_r.grid(row=0, column=2, padx=64)
 
 time_str = StringVar()
 time_str.set('start')
+direction_str = StringVar()
+direction_str.set('barin\n\U0001f815')
 
 def getTime():
     global count
@@ -52,29 +98,38 @@ def getTime():
     button_l.configure(bg="white")
     button_m.configure(bg="white")
     button_r.configure(bg="white")
-
-    if prediction[count] == 0:
-        print('left')
-        button_l.configure(bg="red")
-    elif prediction[count] == 1:
-        print('mid')
-        button_m.configure(bg="gray")
-    elif prediction[count] == 2:
-        print('right')
-        button_r.configure(bg="green")
-    else:
-        raise ValueError
-    
-    root.after(500, getTime)
-    count += 1
+    try:
+        if prediction[count] == 0:
+            direction_str.set('barin\n\U0001f814')
+        elif prediction[count] == 1:
+            direction_str.set('barin\n\U0001f815')
+        elif prediction[count] == 2:
+            direction_str.set('barin\n\U0001f816')
+        else:
+            raise ValueError("Illegal value in prediction {count}")
+        if lights[count] == 0:
+            print('left')
+            button_l.configure(bg="red")
+        elif lights[count] == 1:
+            print('mid')
+            button_m.configure(bg="gray")
+        elif lights[count] == 2:
+            print('right')
+            button_r.configure(bg="green")
+        elif lights[count] == -1:
+            pass
+        else:
+            raise ValueError("Illegal value in lights {count}")
+        
+        root.after(100, getTime)
+        count += 1
+    except:
+        raise RuntimeError("Prediction output done")
+        exit()
 
 def showTime():
     print(time_str.get())
 
-
-# button_l.pack(side=LEFT)
-# button_m.pack(side=LEFT)
-# button_r.pack(side=LEFT)
 
 button_time = Button(but_frame,
                    text = 'right',
@@ -85,6 +140,15 @@ button_time = Button(but_frame,
                    width=10, height=1,
                    command = getTime)
 
-button_time.pack(side=BOTTOM)
+button_predict = Button(top_frame,
+                   textvariable=direction_str,
+                   bg = 'white',
+                   fg = 'black',
+                   font = ('Comic Sans MS', 30),
+                   width=7, height=2)
+
+button_predict.grid(row=0, column=0)
+button_time.grid(row=2, column=0)
+# button_time.pack(side=BOTTOM)
 
 root.mainloop()
